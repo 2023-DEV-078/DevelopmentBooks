41131e7dcddbe922b147a97b0c73b4a2dda3d103
alon55 <alonzager5@gmail.com>
2023-05-18 14:14:04 +0300

    [Refactor]: remove unwanted variables

--- src/constants/testConstants.js
+++ src/constants/testConstants.js
@@ -1,9 +1,7 @@
 export const booksConstants = {
-    ZERO: 0,
     ONE: 1,
     THREE: 3,
     FOUR: 4,
-    BOOK_PRICE: 50,
     CLEAN_CODE: 'Clean Code',
     THE_CLEAN_CODER: 'The Clean Coder',
     CLEAN_ARCHITECTURE: 'Clean Architecture'
