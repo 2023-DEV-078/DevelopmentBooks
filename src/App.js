f63a9adf5d123759d2f0637977e21f2779de9f9c
alon55 <alonzager5@gmail.com>
2023-05-18 13:10:53 +0300

    [Feat]: calculate one book price without discount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cleanCodeQuantity, setCleanCodeQuantity] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const calculateBooksPrice = () => {
+    if (cleanCodeQuantity === 1) { setTotalPrice(50) }
+  }
+
   return (
     <div className="App">
       <h3>
-        Book Price Calculator - TDD
+        Book price calculator - TDD
       </h3>
+      <section className='booksContainer'>
+        <div className='bookInput'>
+          <label htmlFor="clean-code">Clean Code</label>
+          <input type="number" min={0} id="clean-code" value={cleanCodeQuantity} onChange={e => setCleanCodeQuantity(Number(e.target.value))}></input>
+        </div>
+        <button onClick={() => calculateBooksPrice()}>Calculate Total Price</button>
+        <h4>{`Total price: ${totalPrice}`}</h4>
+      </section>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe("Book price calculator - Tests", () => {
@@ -7,6 +7,19 @@ describe("Book price calculator - Tests", () => {
     render(<App />);
     const title = screen.getByRole('heading', { level: 3 });
     expect(title.innerHTML).toBe('Book price calculator - TDD');
+  });
+
+  test("When user buy one book without discount", async () => {
+    render(<App />)
+
+    const input = screen.getByLabelText('Clean Code')
+    fireEvent.change(input, { target: { value: 1 } })
+
+    const calculatePrice = screen.getByRole('button', { name: /Calculate Total Price/i });
+    fireEvent.click(calculatePrice)
+
+    const totalPrice = screen.getByRole('heading', { level: 4 });
+    expect(totalPrice.innerHTML).toBe('Total price: 50');
   });
 
 });
